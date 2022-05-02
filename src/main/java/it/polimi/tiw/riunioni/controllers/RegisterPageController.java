1ae292420fca18c1450a4aa299114c0fabdb1ed7
nicolasbenatti <nicolas.benatti@mail.polimi.it>
2022-05-02 16:09:20 +0200

    added servlet behind register.html

---
+++ src/main/java/it/polimi/tiw/riunioni/controllers/RegisterPageController.java
@@ -0,0 +1,58 @@
+package it.polimi.tiw.riunioni.controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.tiw.riunioni.beans.RegisterErrorBean;
+import it.polimi.tiw.riunioni.utils.ConnectionHandler;
+
+/**
+ * This Servlet has to sole function to process the page template
+ * filling in blank values
+ */
+@WebServlet("/register.html")
+public class RegisterPageController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection conn;   
+    private TemplateEngine templateEngine;   
+	
+    public RegisterPageController() {
+        super();
+    }
+    
+    public void init() throws ServletException {
+    	ServletContext servletContext = getServletContext();
+		this.conn = ConnectionHandler.getConnection(servletContext);
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		String path = "register.html";
+		
+		ctx.setVariable("errorMsg", "");
+		ctx.setVariable("error", new RegisterErrorBean());
+		this.templateEngine.process(path, ctx, response.getWriter());
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException ,IOException {
+		doGet(req, resp);
+	}
+}
